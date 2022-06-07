c4581ab9af27801f366a0e6556845ce5b26820b1
João Vitor Fernandes <joaovitorcarvalhofernandes@hotmail.com>
2022-06-07 17:09:44 -0300

    Atualização

--- src/main/java/com/generation/farmacia/controller/CategoriaController.java
+++ src/main/java/com/generation/farmacia/controller/CategoriaController.java
@@ -2,11 +2,18 @@ package com.generation.farmacia.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,9 +39,36 @@ public class CategoriaController {
 				.map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.notFound().build());
 				}
-	
-	
-	
-	
+	 
+	 @GetMapping("/descricao/{descricao}")
+		public ResponseEntity<List<Categoria>> getByDescricao(@PathVariable String descricao) {
+			return ResponseEntity.ok(categoriaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
+		}
+		
+		@PostMapping
+		public ResponseEntity<Categoria> postCategoria (@Valid @RequestBody Categoria categoria) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+		}
+		
+		@PutMapping
+		public ResponseEntity<Categoria> putCategoria(@Valid @RequestBody Categoria categoria) {
+						
+			return categoriaRepository.findById(categoria.getId())
+					.map(resposta -> {
+						return ResponseEntity.ok().body(categoriaRepository.save(categoria));
+					})
+					.orElse(ResponseEntity.notFound().build());
+		}
 
-}
+		@DeleteMapping("/{id}")
+		public ResponseEntity<?> deleteCategoria(@PathVariable Long id) {
+			
+			return categoriaRepository.findById(id)
+					.map(resposta -> {
+						categoriaRepository.deleteById(id);
+						return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+					})
+					.orElse(ResponseEntity.notFound().build());
+		}
+		
+	 }
